653a7818d7a490d6c9f96a9bacbeae81921d98a2
Damico <raiseDshields@gmail.com>
2018-03-13 18:20:46 -0700

    Changes databse info in settings.py

--- csc648team13/csc648team13/settings.py
+++ csc648team13/csc648team13/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'csc648team13.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'parksgogreen',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
