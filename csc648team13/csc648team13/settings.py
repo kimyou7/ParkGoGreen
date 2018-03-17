4fddc077778193fa5965da354ae643d15b96fd13
Shovels <raiseDshields@gmail.com>
2018-03-17 00:07:16 -0700

    Reverted, will change databse settings when in use

--- csc648team13/csc648team13/settings.py
+++ csc648team13/csc648team13/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'csc648team13.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'parksgogreen',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -111,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
