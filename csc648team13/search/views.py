5cbfa119930c43fe5a2f23c57cdc5a3ce5d821b3
Shovels <raiseDshields@gmail.com>
2018-03-21 06:56:04 -0700

    Got query working

--- csc648team13/search/views.py
+++ csc648team13/search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.urls import reverse
+
+from .models import Park
 
 # Create your views here.
 
@@ -10,9 +11,9 @@ def index(request):
 
 
 def results(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        reports = Park.objects.filter(name__icontains=q)
+        return render(request, 'search/search_results.html', {'reports': reports, 'query': q})
     else:
-        message = 'You submitted an empty form'
-    return HttpResponse(message)
-
+        return HttpResponse('Please submit a search term.')
