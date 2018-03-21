967af31402836871dbc9a5ad6598478bad78708e
Shovels <raiseDshields@gmail.com>
2018-03-21 06:33:58 -0700

    basic search bar and results

--- csc648team13/search/views.py
+++ csc648team13/search/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'search/index.html')
+
+
+def results(request):
+    if 'q' in request.GET:
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty form'
+    return HttpResponse(message)
+
