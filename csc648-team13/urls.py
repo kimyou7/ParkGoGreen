693798b1dc001c2fb8b5e6e1dabff41b5e35fa9c
Shovels <raiseDshields@gmail.com>
2018-03-29 19:49:40 -0700

    Added static files when in debug mode

--- csc648-team13/urls.py
+++ csc648-team13/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('search.urls')),
     #path('ParksGoGreen/', include('ParksGoGreen.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
