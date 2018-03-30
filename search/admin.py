58b5250c1503d182b7ca560c649793f4a666ec1b
Shovels <raiseDshields@gmail.com>
2018-03-30 15:38:07 -0700

    Updated admin

--- search/admin.py
+++ search/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Status, Park
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Status)
+admin.site.register(Park)
