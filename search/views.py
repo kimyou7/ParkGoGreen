7f9edae8fd78e1ee94105d7c27ba3e1828d5ada8
Jaimes Subroto <jaimes_subroto@yahoo.com>
2018-03-29 21:36:10 -0700

    Added blank search to display all items in the db

--- search/views.py
+++ search/views.py
@@ -3,12 +3,8 @@ from django.http import HttpResponse
 
 from .models import Park
 
-# Create your views here.
-
-
 def index(request):
     return render(request, 'search/index.html')
-
 
 def results(request):
     if 'q' in request.GET and request.GET['q']:
@@ -16,4 +12,6 @@ def results(request):
         reports = Park.objects.filter(name__icontains=q)
         return render(request, 'search/search_results.html', {'reports': reports, 'query': q})
     else:
-        return HttpResponse('Please submit a search term.')
+        q = ""
+        reports = Park.objects.all()
+        return render(request, 'search/search_results.html', {'reports': reports, 'query': q})
