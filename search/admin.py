8f17649fab41867c5bef28cc6ac6fc09bcb7d89f
Shovels <raiseDshields@gmail.com>
2018-03-30 19:14:40 -0700

    New models

--- search/admin.py
+++ search/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Category, Status, Park
+from .models import Category, Status, Park, Report
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Status)
 admin.site.register(Park)
+admin.site.register(Report)
--- search/models.py
+++ search/models.py
@@ -6,15 +6,21 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.utils import timezone
 
 
 class Park(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
+    maps_string = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.maps_string:
+            self.maps_string = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBMDvz4zozQwoRPNcXrFX8OCGDp6c1FL7E&q=" + self.name.replace(" ", "+")
+        super(Park, self).save(*args, **kwargs)
 
 
 class Category(models.Model):
@@ -29,3 +35,15 @@ class Status(models.Model):
 
     def __str__(self):
         return self.current_status
+
+
+class Report(models.Model):
+    sub_date = models.DateTimeField('date submitted')
+    description = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='issues')
+    type = models.ForeignKey(Category, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    park = models.ForeignKey(Park, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.park.name + " " + self.sub_date
