583d0fac2a0aeb6c411acc671526f29f133fd834
Shovels <raiseDshields@gmail.com>
2018-04-23 03:53:18 -0700

    Basic log in/logout pages

--- csc648-team13/urls.py
+++ csc648-team13/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('search.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG is True:
