835beccc38d56fce2b00b0f4c7f2f6b46ad0bab7
Shovels <raiseDshields@gmail.com>
2018-04-24 02:49:16 -0700

    Added registration page

--- csc648-team13/urls.py
+++ csc648-team13/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('search.urls')),
     path('admin/', admin.site.urls),
-#    path('accounts/', include('search.urls')),
+    path('accounts/', include('search.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
--- search/views.py
+++ search/views.py
@@ -34,4 +34,4 @@ class ReportDetailView(generic.DetailView):
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
