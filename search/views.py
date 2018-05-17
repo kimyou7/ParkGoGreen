71decb3bb9e5df8dde64dd7a1ad2b53ec3d08d99
Shovels <raiseDshields@gmail.com>
2018-05-17 16:29:09 -0700

    Changed sign up redirect

--- search/views.py
+++ search/views.py
@@ -104,7 +104,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('search:index')
     else:
         form = SignUpForm()
     return render(request, 'search/signup.html', {'form': form})
