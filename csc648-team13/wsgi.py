9e73ed9f6085dac1abfc359205360373fa0f8c95
Shovels <raiseDshields@gmail.com>
2018-05-17 17:13:09 -0700

    Edited wsgi file to be accurate.

--- csc648-team13/wsgi.py
+++ csc648-team13/wsgi.py
@@ -12,8 +12,8 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/opt/bitnami/apps/django/django_projects/PROJECT')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/PROJECT/egg_cache")
+sys.path.append('/opt/bitnami/apps/django/django_projects//opt/bitnami/apps/django/django_projects/csc648-team13')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects//opt/bitnami/apps/django/django_projects/csc648-team13/egg_cache")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csc648-team13.settings")
 
