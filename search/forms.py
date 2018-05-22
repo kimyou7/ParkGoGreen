8f9764b7669a61551c09cf4f04ba9dce1aa0926f
Shovels <raiseDshields@gmail.com>
2018-05-22 00:13:23 -0700

    Replaced signup form with bootstrap-form

--- search/forms.py
+++ search/forms.py
@@ -31,9 +31,9 @@ class PostForm(ModelForm):
 
 # Sign up form with added email field and optional name fields.
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required.')
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    email = forms.EmailField(max_length=254, help_text='Required')
 
     class Meta:
         model = User
